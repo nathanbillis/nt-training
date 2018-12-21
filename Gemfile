03920dca32a287cdc4d9d019147e8ed76ca408a1
Nathan Billis <nathanbillis.nathan@gmail.com>
2018-12-21 18:37:37 +0000

    pls

--- Gemfile
+++ Gemfile
@@ -1,5 +1,5 @@
 source "https://rubygems.org"
-
+ruby '2.4.5'
 gem 'compass'
 gem 'bootstrap', '~> 4.0.0.beta'
 gem 'sass-rails', '>= 3.2'
--- training/settings.py
+++ training/settings.py
@@ -15,21 +15,30 @@ import django_heroku
 
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool(int(os.environ.get('DEBUG'))) if os.environ.get('DEBUG') else True
 
 # SECURITY WARNING: keep the secret key used in production secret!
-if not DEBUG:
-    SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
-else:
-    with open('secret_key.txt') as f:
-        SECRET_KEY = f.read().strip()
+
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
+
 
 ALLOWED_HOSTS = ['training.samosborne.me', 'training-york-tech.herokuapp.com', '127.0.0.1', 'localhost']
 
 
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 # Application definition
 
 INSTALLED_APPS = [
